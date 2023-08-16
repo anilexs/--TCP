dab81cdddac1b1b29c45c251d79fc573a164d7cc
alexis <alexissimoes94@gmail.com>
2023-08-16 11:04:06 +0200

    v34

--- arcade/mercenaire_mystere/js/mercenaire_mystere.js
+++ arcade/mercenaire_mystere/js/mercenaire_mystere.js
@@ -1,44 +1,63 @@
-function rogue (id){
+var checkId = [anvil, cannon, chaac, dahlia, dallas, dima, fixer, gl1tch, glimpse, juke, kestrel, lancer, mack, phantom, ronin, runway, saint, scorch, seeker, sigrid, switchblade, talon, trench, umbra, vivi, vy]
+var divId = ["anvilDiv", "cannonDiv", "chaacDiv", "dahliaDiv", "dallasDiv", "dimaDiv", "fixerDiv", "gl1tchDiv", "glimpseDiv", "jukeDiv", "kestrelDiv", "lancerDiv", "mackDiv", "phantomDiv", "roninDiv", "runwayDiv", "saintDiv", "scorchDiv", "seekerDiv", "sigridDiv", "switchbladeDiv", "talonDiv", "trenchDiv", "umbraDiv", "viviDiv", "vyDiv"]
+
+
+$("#all").change(function() {
+        if ($("#all").is(":checked")) {
+            for (i = 0; i < checkId.length; i++){
+                $(checkId[i]).prop("checked", true);
+            }
+        } else {
+            for (i = 0; i < checkId.length; i++){
+                $(checkId[i]).prop("checked", false);
+                $("#" + divId[i]).remove();
+            }
+        }
+    });
+
+
+
+function rogue (id, divId, img){
     $(id).change(function() {
         if ($(id).is(":checked")) {
-            console.log("La case est cochée !");
+            var div = $('<div id="' + divId + '" class="img"></div>');
+            $('#select').append(div);
         } else {
-            console.log("La case est décochée !");
+            $("#" + divId).remove();
         }
     });
 }
 
-
-// rogue(all);
 // rogue(soutien);
 // rogue(défenseur);
 // rogue(premiére_ligne);
 // rogue(duellist);
 // rogue(renseignement);
-rogue(anvil);
-rogue(cannon);
-rogue(chaac);
-rogue(dahlia);
-rogue(dallas);
-rogue(dima);
-rogue(fixer);
-rogue(gl1tch);
-rogue(glimpse);
-rogue(juke);
-rogue(kestrel);
-rogue(lancer);
-rogue(mack);
-rogue(phantom);
-rogue(ronin);
-rogue(runway);
-rogue(saint);
-rogue(scorch);
-rogue(seeker);
-rogue(sigrid);
-rogue(switchblade);
-rogue(talon);
-rogue(trench);
-rogue(umbra);
-rogue(vivi);
-rogue(vy);
+
+rogue(anvil, "anvilDiv");
+rogue(cannon, "cannonDiv");
+rogue(chaac, "chaacDiv");
+rogue(dahlia, "dahliaDiv");
+rogue(dallas, "dallasDiv");
+rogue(dima, "dimaDiv");
+rogue(fixer, "fixerDiv");
+rogue(gl1tch, "gl1tchDiv");
+rogue(glimpse, "glimpseDiv");
+rogue(juke, "jukeDiv");
+rogue(kestrel, "kestrelDiv");
+rogue(lancer, "lancerDiv");
+rogue(mack, "mackDiv");
+rogue(phantom, "phantomDiv");
+rogue(ronin, "roninDiv");
+rogue(runway, "runwayDiv");
+rogue(saint, "saintDiv");
+rogue(scorch, "scorchDiv");
+rogue(seeker, "seekerDiv");
+rogue(sigrid, "sigridDiv");
+rogue(switchblade, "switchbladeDiv");
+rogue(talon, "talonDiv");
+rogue(trench, "trenchDiv");
+rogue(umbra, "umbraDiv");
+rogue(vivi, "viviDiv");
+rogue(vy, "vyDiv");
 
