900b7ba965866e623d23bb31fdee1538d38a930f
alexis <alexissimoes94@gmail.com>
2023-08-16 13:55:05 +0200

    v35

--- arcade/mercenaire_mystere/js/mercenaire_mystere.js
+++ arcade/mercenaire_mystere/js/mercenaire_mystere.js
@@ -1,34 +1,114 @@
-var checkId = [anvil, cannon, chaac, dahlia, dallas, dima, fixer, gl1tch, glimpse, juke, kestrel, lancer, mack, phantom, ronin, runway, saint, scorch, seeker, sigrid, switchblade, talon, trench, umbra, vivi, vy]
-var divId = ["anvilDiv", "cannonDiv", "chaacDiv", "dahliaDiv", "dallasDiv", "dimaDiv", "fixerDiv", "gl1tchDiv", "glimpseDiv", "jukeDiv", "kestrelDiv", "lancerDiv", "mackDiv", "phantomDiv", "roninDiv", "runwayDiv", "saintDiv", "scorchDiv", "seekerDiv", "sigridDiv", "switchbladeDiv", "talonDiv", "trenchDiv", "umbraDiv", "viviDiv", "vyDiv"]
+// var checkId = [anvil, cannon, chaac, dahlia, dallas, dima, fixer, gl1tch, glimpse, juke, kestrel, lancer, mack, phantom, ronin, runway, saint, scorch, seeker, sigrid, switchblade, talon, trench, umbra, vivi, vy];
+// let divId = ["anvilDiv", "cannonDiv", "chaacDiv", "dahliaDiv", "dallasDiv", "dimaDiv", "fixerDiv", "gl1tchDiv", "glimpseDiv", "jukeDiv", "kestrelDiv", "lancerDiv", "mackDiv", "phantomDiv", "roninDiv", "runwayDiv", "saintDiv", "scorchDiv", "seekerDiv", "sigridDiv", "switchbladeDiv", "talonDiv", "trenchDiv", "umbraDiv", "viviDiv", "vyDiv"];
+// let soutien = [dahlia, saint];
+// let soutienId = ["dahliaDiv", "saintDiv"];
+let tabAssoc = {
+    "anvil" : "anvilDiv",
+    "cannon" : "cannonDiv",
+    "chaac" : "chaacDiv",
+    "dahlia" : "dahliaDiv",
+    "dallas" : "dallasDiv",
+    "dima" : "dimaDiv",
+    "fixer" : "fixerDiv",
+    "gl1tch" : "gl1tchDiv",
+    "glimpse" : "glimpseDiv",
+    "juke" : "jukeDiv",
+    "kestrel" : "kestrelDiv",
+    "lancer" : "lancerDiv",
+    "mack" : "mackDiv",
+    "phantom" : "phantomDiv",
+    "ronin" : "roninDiv",
+    "runway" : "runwayDiv",
+    "saint" : "saintDiv",
+    "scorch" : "scorchDiv",
+    "seeker" : "seekerDiv",
+    "sigrid" : "sigridDiv",
+    "switchblade" : "switchbladeDiv",
+    "talon" : "talonDiv",
+    "trench" : "trenchDiv",
+    "umbra" : "umbraDiv",
+    "vivi" : "viviDiv",
+    "vy" : "vyDiv",
+    "soutien" : {
+        "dahlia" : "dahliaDiv",
+        "saint" : "saintDiv",
+    }
+}
 
+// soutienId.forEach((element) => console.log(element));
+// Object.keys(tabAssoc).forEach(function(cle) {
+//   const valeur = tabAssoc[cle];
+//   console.log(`Clé: ${cle}, Valeur: ${valeur}`);
+// });
+
+// $("#all").change(function() {
+//     if ($("#all").is(":checked")) {
+
+// Object.keys(tabAssoc).forEach(function(cle) {
+//   const valeur = tabAssoc[cle];
+//   if (!$("#" + valeur).length) {
+//                 $(cle).prop("checked", true);
+//                 var div = $('<div id="' + valeur + '" class="img"></div>');
+//                 $('#select').append(div);
+//                 console.log($("#" + valeur).length);
+//             }
+// }); 
+// }
+// });
 
 $("#all").change(function() {
-        if ($("#all").is(":checked")) {
-            for (i = 0; i < checkId.length; i++){
-                $(checkId[i]).prop("checked", true);
-            }
-        } else {
-            for (i = 0; i < checkId.length; i++){
-                $(checkId[i]).prop("checked", false);
-                $("#" + divId[i]).remove();
-            }
+    if ($("#all").is(":checked")) {
+      Object.keys(tabAssoc).forEach(function(cle) {
+        const valeur = tabAssoc[cle];
+        if (!$("#" + valeur).length) {
+          $("#" + cle).prop("checked", true);
+          var div = $('<div id="' + valeur + '" class="img"></div>');
+          $('#select').append(div);
         }
-    });
+      });
+    } else {
+      Object.keys(tabAssoc).forEach(function(cle) {
+        const valeur = tabAssoc[cle];
+        $("#" + cle).prop("checked", false);
+        $("#" + valeur).remove();
+      });
+    }
+  });
 
+ $("#soutien").change(function() {
+    const soutien = tabAssoc["soutien"]; // Accéder à la sous-structure "soutien"
 
+    if ($("#soutien").is(":checked")) {
+      Object.keys(soutien).forEach(function(cle) {
+        const valeur = soutien[cle];
+        if (!$("#" + valeur).length) {
+          $("#" + cle).prop("checked", true);
+          var div = $('<div id="' + valeur + '" class="img"></div>');
+          $('#select').append(div);
+        }
+      });
+    } else {
+      Object.keys(soutien).forEach(function(cle) {
+        const valeur = soutien[cle];
+        $("#" + cle).prop("checked", false);
+        $("#" + valeur).remove();
+      });
+    }
+  });
 
-function rogue (id, divId, img){
+function rogue(id, divId, role, img) {
     $(id).change(function() {
         if ($(id).is(":checked")) {
-            var div = $('<div id="' + divId + '" class="img"></div>');
-            $('#select').append(div);
+            if (!$("#" + divId).length) {
+                var div = $('<div id="' + divId + '" class="img"></div>');
+                $('#select').append(div);
+            }
         } else {
             $("#" + divId).remove();
         }
     });
 }
 
-// rogue(soutien);
 // rogue(défenseur);
 // rogue(premiére_ligne);
 // rogue(duellist);
@@ -37,7 +117,7 @@ function rogue (id, divId, img){
 rogue(anvil, "anvilDiv");
 rogue(cannon, "cannonDiv");
 rogue(chaac, "chaacDiv");
-rogue(dahlia, "dahliaDiv");
+rogue(dahlia, "dahliaDiv", "soutien");
 rogue(dallas, "dallasDiv");
 rogue(dima, "dimaDiv");
 rogue(fixer, "fixerDiv");
@@ -50,7 +130,7 @@ rogue(mack, "mackDiv");
 rogue(phantom, "phantomDiv");
 rogue(ronin, "roninDiv");
 rogue(runway, "runwayDiv");
-rogue(saint, "saintDiv");
+rogue(saint, "saintDiv" , "soutien");
 rogue(scorch, "scorchDiv");
 rogue(seeker, "seekerDiv");
 rogue(sigrid, "sigridDiv");
@@ -60,4 +140,3 @@ rogue(trench, "trenchDiv");
 rogue(umbra, "umbraDiv");
 rogue(vivi, "viviDiv");
 rogue(vy, "vyDiv");
-
